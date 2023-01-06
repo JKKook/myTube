e73a76c0d2f66c8475bd141cd2f4b4540a853c1f
JKKook <whdrnrdldia@naver.com>
2023-01-06 11:24:09 +0900

    FEAT Implementing VideoCard components

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import Header from '../components/Header';
 import { Outlet } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 export default function Root() {
   return (
     <div>
       <div>
         <Header />
-        <Outlet />
+        {/* query로 router의 outlet을 감싸 줌 */}
+        <QueryClientProvider client={queryCilent}>
+          <Outlet />
+        </QueryClientProvider>
       </div>
     </div>
   );
 }
+
+const queryCilent = new QueryClient();
--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,7 +1,42 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import VideoCard from '../components/VideoCard';
 
 export default function Videos() {
   const { keyword } = useParams();
-  return <div>videos {keyword ? `🔎${keyword}` : '😋'}</div>;
+
+  const {
+    isLoading,
+    error,
+    // videos 배열에 관심
+    data: videos,
+    // useQuery cache
+  } = useQuery({
+    // key : videos , variable : keyword
+    queryKey: ['videos', keyword],
+    queryFn: async () => {
+      // 경로에 대해서 자세하게 살펴볼 필요가 있다!
+      return fetch(
+        `/data/${keyword ? 'list-by-keyword' : 'list-by-popular'}.json`,
+      )
+        .then((res) => res.json())
+        .then((data) => data.items);
+    },
+  });
+
+  return (
+    <>
+      <div>Videos {keyword ? `🔎${keyword}` : '😋'}</div>
+      {isLoading && <p>Loading...</p>}
+      {error && <p>Something is wrong</p>}
+      {videos && (
+        <ul>
+          {videos.map((video) => (
+            <VideoCard key={video.id} video={video} />
+          ))}
+        </ul>
+      )}
+    </>
+  );
 }
